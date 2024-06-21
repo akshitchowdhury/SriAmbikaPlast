0bdbf2729b6ce49d9d4e4a632b638882faf363ac
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-21 18:12:39 +0530

    contactFrom2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import './Contact.css';
+import ContactForm from './ContactForm';
 
 const Contact = () => {
   return (
     <div className="contact h-auto md:h-auto flex flex-col md:flex-row md:space-x-10 space-y-10 md:space-y-0 p-6 md:p-14">
       {/* Left section with Contact Form */}
-      <div className="left w-full md:w-1/2 p-6 md:p-10 rounded-md bg-amber-400 bg-opacity-50 flex flex-col space-y-6">
-        <h2 className="text-lg md:text-3xl text-white font-medium">Get in Touch</h2>
-        <form className="flex flex-col space-y-4">
-          <div className="flex flex-col">
-            <label htmlFor="name" className="text-white mb-2">Name</label>
-            <input type="text" id="name" name="name" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="email" className="text-white mb-2">Email</label>
-            <input type="email" id="email" name="email" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="message" className="text-white mb-2">Message</label>
-            <textarea id="message" name="message" rows="5" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required></textarea>
-          </div>
-          <button type="submit" className="p-3 rounded-md bg-white text-purple-700 font-semibold hover:bg-purple-100 focus:outline-none focus:ring-2 focus:ring-purple-500">
-            Submit
-          </button>
-        </form>
-      </div>
+      <ContactForm/>
       
       {/* Right section */}
       <div className="right w-full md:w-1/2 p-4 space-y-4">
---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div className="left w-full md:w-1/2 p-6 md:p-10 rounded-md bg-orange-500  flex flex-col space-y-6">
+        <h2 className="text-lg md:text-3xl text-white font-medium">Get in Touch</h2>
+        <form className="flex flex-col space-y-4">
+          <div className="flex flex-col">
+            <label htmlFor="name" className="text-white mb-2">Name</label>
+            <input type="text" id="name" name="name" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-white mb-2">Email</label>
+            <input type="email" id="email" name="email" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required />
+          </div>
+          <div className='flex flex-row gap-28'>
+          <div className="flex flex-col">
+            <label htmlFor="phone" className="text-white mb-2">Phone</label>
+            <input type="number" id="phone" name="phone" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="message" className="text-white mb-2">Message</label>
+            <textarea id="message" name="message" rows="2" className="p-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500" required></textarea>
+          </div>
+          </div>
+          <button type="submit" className="p-3 rounded-md bg-white text-purple-700 font-semibold hover:bg-purple-100 focus:outline-none focus:ring-2 focus:ring-purple-500">
+            Submit
+          </button>
+        </form>
+      </div>
+  )
+}
+
+export default ContactForm
