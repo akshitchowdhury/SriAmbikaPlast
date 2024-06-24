a045f8788dfe5810db12ea393ddece6c98254f84
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-24 13:21:12 +0530

    DataMaking2

---
+++ src/components/Product/productData.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    category1: "PVC Suction Hose Pipe",
+    subproducts: [
+      {
+        product1: {
+          ProductName: "Flex Suction",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: 4.5 }],
+          colors: ["Green", "Blue", "Orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product2: {
+          ProductName: "Super Flex Suction",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          colors: ["Parrot green", "Blue", "Gray", "White"],
+          rollLength: "30mtr",
+        },
+      },
+    ],
+  },
+  {
+    category2: "PVC Braided Hose Pipe",
+    subproducts: [
+      {
+        product1: {
+          ProductName: "Top Lon Braided",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: 4.5 }],
+          colors: ["green", "yellow", "orange", "Parrot Green", "Pink"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product2: {
+          ProductName: "Good Lon Braided",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr, 50mtr, 100mtr",
+        },
+      },
+    ],
+  },
+  {
+    category3: "Garden and Tubing Hose Pipe",
+    subproducts: [
+      {
+        product1: {
+          ProductName: "Fifty-Fifty (50-50)",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: 4.5 }],
+          colors: [ "Yellow", "Orange", "Parrot Green", "Pink"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product2: {
+          ProductName: "Color Transparent (C.T.T)",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Yellow", "Orange", "Parrot Green", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product3: {
+          ProductName: "Color Zebra",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product4: {
+          ProductName: "Opek",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+    ],
+  },
+];
