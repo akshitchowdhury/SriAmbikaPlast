596be0325638ac7aa9eabc27265c60f56ab79d86
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-24 13:18:08 +0530

    DataMaking

--- src/components/Home/What We Offer/WhatWeOffer.jsx
+++ src/components/Home/What We Offer/WhatWeOffer.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import whatWeOffer from "../../../assets/whatWeOffer.jpg"
+import React from "react";
+import whatWeOffer from "../../../assets/whatWeOffer.jpg";
 const WhatWeOffer = () => {
   return (
     <>
-    
-    <section className="py-8 px-4 md:py-16 md:px-8 bg-gray-100">
-    <h1 className='text-left font-semibold text-4xl text-gray-700 py-4'>What We Offfer</h1>
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
-        
-        <div className="flex-1 flex justify-center md:justify-start">
-          <img src={whatWeOffer} alt="Source from India" className="max-w-full h-auto" />
-        </div>
-        
-        <div className="flex-1">
-          <div className="flex flex-col items-start">
-            <h1 className="text-3xl md:text-4xl font-semibold mb-6 text-center md:text-left">Source from India</h1>
-            <p className="text-lg md:text-xl text-gray-600 mb-6 text-center md:text-left">
-              Our supplier outreach initiative is part of our overall business strategy to add value to the communities we serve. Company-wide, our employees share a commitment to being the leader in providing our customers and communities with traditional and innovative utility services, and Supplier Outreach is one of the ways we accomplish this.
-            </p>
+      <section className="py-8 px-4 md:py-16 md:px-8 bg-gray-100">
+        <h1 className="text-left font-semibold text-4xl  py-4">
+          What We Offfer
+        </h1>
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
+          <div className="flex-1 flex justify-center md:justify-start">
+            <img
+              src={whatWeOffer}
+              alt="Source from India"
+              className="max-w-full h-auto"
+            />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col items-start">
+              <h1 className="text-3xl md:text-4xl font-semibold mb-6 text-center md:text-left">
+                Source from India
+              </h1>
+              <p className="text-lg md:text-xl text-gray-600 mb-6 text-center md:text-left">
+                At Sri Ambika Plast, our supplier outreach initiative is an
+                integral part of our overall strategy to add value to the
+                communities we serve. Across our company, our employees are
+                dedicated to leading the way in delivering top-quality piping
+                solutions for water and agricultural applications. By engaging
+                with our suppliers through this initiative, we ensure that we
+                provide our customers and communities with both traditional and
+                innovative piping products. Our supplier outreach is one of the
+                key methods we use to achieve excellence and foster strong,
+                mutually beneficial relationships within the industry.
+              </p>
+            </div>
           </div>
         </div>
-        
-      </div>
-    </section>
+      </section>
     </>
   );
 };
