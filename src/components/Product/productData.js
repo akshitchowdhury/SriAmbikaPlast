3b89a0a995e04c1cb7821cfd10988a568d9ef65b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-24 13:57:31 +0530

    DataMaking3

--- src/components/Product/productData.js
+++ src/components/Product/productData.js
@@ -90,6 +90,105 @@ const products = [
           rollLength: "30mtr",
         },
       },
+      {
+        product5: {
+          ProductName: "White T.T",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product6: {
+          ProductName: "Metallic",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product7: {
+          ProductName: "Black Zebra",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow", "Blue", "White", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product8: {
+          ProductName: "Double Decker",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow", "Blue", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product9: {
+          ProductName: "Super Foam",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product10: {
+          ProductName: "Super 32 Line Rib",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          colors: [ "Parrot Green","Yellow","Blue", "Orange", "Pink"],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product11: {
+          ProductName: "Level Tube",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr",
+        },
+      },
+      
+    ],
+  },
+  {
+    category4: "LD Kissan Hose/Layflat Tube",
+    subproducts: [
+      {
+        product1: {
+          ProductName: "Color L.D. Pipes",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: 4.5 }],
+          colors: ["Dark Green", "Dark Blue", "White", "Black"],
+          rollLength: "30mtr",
+        },
+      },
+      {
+        product2: {
+          ProductName: "White Transparent",
+          image: {},
+          sizeWeight: [{ s1: '3/4"', w1: "5.5" }],
+          //   colors: ["green", "blue", "orange"],
+          rollLength: "30mtr, 50mtr, 100mtr",
+        },
+      },
     ],
   },
 ];
