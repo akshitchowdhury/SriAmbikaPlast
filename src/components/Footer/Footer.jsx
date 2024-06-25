cb190698f5a73eeda3674074d952161f9f6f3378
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-25 17:49:30 +0530

    designs8

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,6 +32,19 @@ const Footer = () => {
           {/* <div className="md:col-span-1">
           <ImportantLinks />
           </div> */}
+          <div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.942406986801!2d77.5116481750765!3d12.97553548734019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3c3296cc28ff%3A0x1a45ef7cde68296e!2s16%2C%2015th%20Cross%20Rd%2C%20Nagarbhavi%20Second%20Stage%2C%20Malagala%2C%20Krishnananda%20Nagar%2C%20Naagarabhaavi%2C%20Bengaluru%2C%20Karnataka%20560072!5e0!3m2!1sen!2sin!4v1719314044310!5m2!1sen!2sin"
+        // width="400"
+        // height="250"
+        style={{ border: 0 }}
+        className="md:-mx-20, h-[200px] w-[130px] md:w-[300px] md:h-[250px], "
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Google Maps Embed"
+      ></iframe>
+      </div>
         </div>
       </div>
       <div className="bg-gray-100 text-gray-800 py-4 md:flex md:items-center md:justify-between">
