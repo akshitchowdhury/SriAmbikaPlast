c3e68b372eb1f300b04aa77aa447530b2cf9d6bc
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-25 12:21:38 +0530

    ProductsInitialized2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,19 @@ import FlexSuction from './components/Product/ProductStack/FlexSuction';
 import SuperFlexSuction from './components/Product/ProductStack/SuperFlexSuction';
 import TopLonBraided from './components/Product/ProductStack/TopLonBraided';
 import GoodLonBraided from './components/Product/ProductStack/GoodLonBraided';
+import FiftyFifty from './components/Product/ProductStack/FifityFifty';
+import ColorTransparent from './components/Product/ProductStack/ColorTransparent';
+import ColorZebra from './components/Product/ProductStack/ColorZebra';
+import Opek from './components/Product/ProductStack/Opek';
+import WhiteTT from './components/Product/ProductStack/WhiteTT';
+import Metallic from './components/Product/ProductStack/Metallic';
+import BlackZebra from './components/Product/ProductStack/BlackZebra';
+import DoubleDecker from './components/Product/ProductStack/DoubleDecker';
+import SuperFoam from './components/Product/ProductStack/SuperFoam';
+import SuperLineRib from './components/Product/ProductStack/SuperLineRib';
+import LevelTube from './components/Product/ProductStack/LevelTube';
+import ColorLDPipes from './components/Product/ProductStack/LD HOse Tube/ColorLDPipes';
+import WhiteTransparent from './components/Product/ProductStack/LD HOse Tube/WhiteTransparent';
 
 
 
@@ -45,6 +58,20 @@ function App() {
           <Route path='/super-flex-suction' element={<SuperFlexSuction/>}/>
           <Route path='/top-lon-braided' element={<TopLonBraided/>}/>
           <Route path='/good-lon-braided' element={<GoodLonBraided/>}/>
+          <Route path='/good-lon-braided' element={<GoodLonBraided/>}/>
+          <Route path='/fifty-fifty-(50-50)' element={<FiftyFifty/>}/>
+          <Route path='/color-transparent-(c.t.t)' element={<ColorTransparent/>}/>
+          <Route path='/color-zebra' element={<ColorZebra/>}/>
+          <Route path='/opek' element={<Opek/>}/>
+          <Route path="/white-t.t" element={<WhiteTT />} />
+          <Route path="/metallic" element={<Metallic />} />
+          <Route path="/black-zebra" element={<BlackZebra />} />
+          <Route path="/double-decker" element={<DoubleDecker/>} />
+          <Route path="/super-foam" element={<SuperFoam/>} />
+          <Route path="/super-32-line-rib" element={<SuperLineRib/>} />
+          <Route path="/level-tube" element={<LevelTube/>} />
+          <Route path="/color-l.d.-pipes" element={<ColorLDPipes/>} />
+          <Route path="/white-transparent" element={<WhiteTransparent/>} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
