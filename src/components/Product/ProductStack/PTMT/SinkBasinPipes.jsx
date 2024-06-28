10bc72501d32796fa6ed9fc739e9d54e7c2c322b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-06-28 15:28:17 +0530

    logoDesign6

--- src/components/Product/ProductStack/PTMT/SinkBasinPipes.jsx
+++ src/components/Product/ProductStack/PTMT/SinkBasinPipes.jsx
@@ -7,6 +7,9 @@ import durable from "../../../../assets/products/strongDurable.png";
 import wearResistant from "../../../../assets/products/wearResistant.webp";
 import ecoFriendly from "../../../../assets/products/ecoFriendly.jpg";
 import vibrantColors from "../../../../assets/products/vibrantColors.png";
+import suctionHose from "../../../../assets/products/Sink pipe/suctionHose.png";
+import coloBraidede from "../../../../assets/products/Sink pipe/coloBraidede.png";
+import gardenTubing from "../../../../assets/products/Sink pipe/gardenTubing.png";
 const SinkBasinPipes = () => {
     const product = productsData[5].subproducts[0].product;
 
@@ -68,13 +71,298 @@ const SinkBasinPipes = () => {
                   </tbody>
                 </table>
               </div>
+             
+             
+             
+             
               <div className="mt-8">
-                <h2 className="text-xl font-bold text-green-600 mb-4">
-                  ROLL LENGTH: {product.rollLength}
-                </h2>
+               
                 <h3 className="text-lg font-semibold text-gray-800 mb-2">
                   Product Quality:
                 </h3>
+
+
+
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
+                    <img
+                      src={durable}
+                      alt="Durability"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">
+                      High durability and flexibility
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-white text-orange-500 rounded-lg">
+                    <img
+                      src={wearResistant}
+                      alt="Wear Resistance"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">
+                      Resistant to wear and tear
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-white text-orange-500 rounded-lg">
+                    <img
+                      src={ecoFriendly}
+                      alt="Eco-friendly"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">Eco-friendly materials</span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
+                    <img
+                      src={vibrantColors}
+                      alt="Vibrant Colors"
+                      className="w-16 h-12 mb-2"
+                    />
+                    <span className="text-center">Multiple vibrant colors</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row items-start mt-6">
+            <div className="w-full md:w-1/2 lg:w-[500px] lg:h-[500px] h-auto object-cover rounded mb-6 md:mb-0">
+              <Slider {...settings}>
+                {product.image.map((imgSrc, index) => (
+                  <div key={index}>
+                    <img
+                      src={suctionHose}
+                      alt={`${product.ProductName} ${index + 1}`}
+                      className="w-full h-[200px] object-contain"
+                    />
+                  </div>
+                ))}
+              </Slider>
+              <h1 className="text-2xl font-bold text-orange-600 mt-4 text-center">
+                Suction Hose
+              </h1>
+            </div>
+            <div className="md:ml-6 w-full md:w-1/2">
+              <div className="overflow-x-auto mb-6">
+              <table className="min-w-full bg-white border border-gray-300">
+                  <thead>
+                    <tr>
+                      <th className="py-3 px-5 bg-orange-600 text-white font-bold uppercase text-lg border-b border-gray-300">Without Cup</th>
+                      <th className="py-3 px-5 bg-orange-600 text-white font-bold uppercase text-lg border-b border-gray-300">With Cup</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="even:bg-orange-200">
+                    <td className="py-3 px-5 text-gray-700 text-center font-semibold text-lg border-r border-gray-300">
+                        
+                        {product.sizeWeight.filter(item => !item.withCup).map(item => `32"-30"`).join(', ')}
+                      </td>
+                      <td className="py-3 px-5 text-gray-700 text-center font-semibold text-lg border-r border-gray-300">
+                        
+                        {product.sizeWeight.filter(item => item.withCup).map(item => `38`).join(', ')}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+             
+             
+             
+             
+              <div className="mt-8">
+               
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">
+                  Product Quality:
+                </h3>
+
+
+
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
+                    <img
+                      src={durable}
+                      alt="Durability"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">
+                      High durability and flexibility
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-white text-orange-500 rounded-lg">
+                    <img
+                      src={wearResistant}
+                      alt="Wear Resistance"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">
+                      Resistant to wear and tear
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-white text-orange-500 rounded-lg">
+                    <img
+                      src={ecoFriendly}
+                      alt="Eco-friendly"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">Eco-friendly materials</span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
+                    <img
+                      src={vibrantColors}
+                      alt="Vibrant Colors"
+                      className="w-16 h-12 mb-2"
+                    />
+                    <span className="text-center">Multiple vibrant colors</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row items-start mt-6">
+            <div className="w-full md:w-1/2 lg:w-[500px] lg:h-[500px] h-auto object-cover rounded mb-6 md:mb-0">
+              <Slider {...settings}>
+                {product.image.map((imgSrc, index) => (
+                  <div key={index}>
+                    <img
+                      src={coloBraidede}
+                      alt={`${product.ProductName} ${index + 1}`}
+                      className="w-full h-[200px] object-contain"
+                    />
+                  </div>
+                ))}
+              </Slider>
+              <h1 className="text-2xl font-bold text-orange-600 mt-4 text-center">
+                Braided Hose
+              </h1>
+            </div>
+            <div className="md:ml-6 w-full md:w-1/2">
+              <div className="overflow-x-auto mb-6">
+              <table className="min-w-full bg-white border border-gray-300">
+                  <thead>
+                    <tr>
+                      <th className="py-3 px-5 bg-orange-600 text-white font-bold uppercase text-lg border-b border-gray-300">Without Cup</th>
+                      <th className="py-3 px-5 bg-orange-600 text-white font-bold uppercase text-lg border-b border-gray-300">With Cup</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="even:bg-orange-200">
+                    <td className="py-3 px-5 text-gray-700 text-center font-semibold text-lg border-r border-gray-300">
+                        
+                        {product.sizeWeight.filter(item => !item.withCup).map(item => `42"-40"`).join(', ')}
+                      </td>
+                      <td className="py-3 px-5 text-gray-700 text-center font-semibold text-lg border-r border-gray-300">
+                        
+                        {product.sizeWeight.filter(item => item.withCup).map(item => `48`).join(', ')}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+             
+             
+             
+             
+              <div className="mt-8">
+               
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">
+                  Product Quality:
+                </h3>
+
+
+
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
+                    <img
+                      src={durable}
+                      alt="Durability"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">
+                      High durability and flexibility
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-white text-orange-500 rounded-lg">
+                    <img
+                      src={wearResistant}
+                      alt="Wear Resistance"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">
+                      Resistant to wear and tear
+                    </span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-white text-orange-500 rounded-lg">
+                    <img
+                      src={ecoFriendly}
+                      alt="Eco-friendly"
+                      className="w-12 h-12 mb-2"
+                    />
+                    <span className="text-center">Eco-friendly materials</span>
+                  </div>
+                  <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
+                    <img
+                      src={vibrantColors}
+                      alt="Vibrant Colors"
+                      className="w-16 h-12 mb-2"
+                    />
+                    <span className="text-center">Multiple vibrant colors</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row items-start mt-6">
+            <div className="w-full md:w-1/2 lg:w-[500px] lg:h-[500px] h-auto object-cover rounded mb-6 md:mb-0">
+              <Slider {...settings}>
+                {product.image.map((imgSrc, index) => (
+                  <div key={index}>
+                    <img
+                      src={gardenTubing}
+                      alt={`${product.ProductName} ${index + 1}`}
+                      className="w-full h-[200px] object-contain"
+                    />
+                  </div>
+                ))}
+              </Slider>
+              <h1 className="text-2xl font-bold text-orange-600 mt-4 text-center">
+                Tubing Hose
+              </h1>
+            </div>
+            <div className="md:ml-6 w-full md:w-1/2">
+              <div className="overflow-x-auto mb-6">
+              <table className="min-w-full bg-white border border-gray-300">
+                  <thead>
+                    <tr>
+                      <th className="py-3 px-5 bg-orange-600 text-white font-bold uppercase text-lg border-b border-gray-300">Without Cup</th>
+                      <th className="py-3 px-5 bg-orange-600 text-white font-bold uppercase text-lg border-b border-gray-300">With Cup</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="even:bg-orange-200">
+                    <td className="py-3 px-5 text-gray-700 text-center font-semibold text-lg border-r border-gray-300">
+                        
+                        {product.sizeWeight.filter(item => !item.withCup).map(item => `32"-30"`).join(', ')}
+                      </td>
+                      <td className="py-3 px-5 text-gray-700 text-center font-semibold text-lg border-r border-gray-300">
+                        
+                        {product.sizeWeight.filter(item => item.withCup).map(item => `38`).join(', ')}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+             
+             
+             
+             
+              <div className="mt-8">
+               
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">
+                  Product Quality:
+                </h3>
+
+
+
                 <div className="grid grid-cols-2 gap-4">
                   <div className="flex flex-col items-center p-4 bg-orange-500 text-white rounded-lg">
                     <img
